ca1f9e91a4067579e14f935a2428e7e524ff8903
Arpit Gupta <agsinghgarg131@gmail.com>
2021-05-08 23:28:30 +0530

    Updates

---
+++ precache-manifest.95fdaaad0fbed9f753e577b9fe9811e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1738c90275146fb92ef190dd76ff28",
+    "url": "/arpit_gupta/index.html"
+  },
+  {
+    "revision": "6d27548b69fd9d09df6b",
+    "url": "/arpit_gupta/static/css/main.42513db5.chunk.css"
+  },
+  {
+    "revision": "0ffde44b669ba29484da",
+    "url": "/arpit_gupta/static/js/2.e2d8739b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/arpit_gupta/static/js/2.e2d8739b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d27548b69fd9d09df6b",
+    "url": "/arpit_gupta/static/js/main.956e2a99.chunk.js"
+  },
+  {
+    "revision": "ce40bb65f4e6961f1658",
+    "url": "/arpit_gupta/static/js/runtime-main.995bf582.js"
+  },
+  {
+    "revision": "85ba870fb476a8b2e08c31270da8e411",
+    "url": "/arpit_gupta/static/media/arpitgupta.85ba870f.jpg"
+  },
+  {
+    "revision": "6660f2b6fbe5d49c2f235f143c17af11",
+    "url": "/arpit_gupta/static/media/sigaram.6660f2b6.jpg"
+  },
+  {
+    "revision": "e019168347007abd78384e6a9259f4da",
+    "url": "/arpit_gupta/static/media/vkarma.e0191683.png"
+  }
+]);
