bf7f68c3dff28c9f0bc12ed665550f9c617b25cb
Arpit Gupta <agsinghgarg131@gmail.com>
2021-05-09 00:14:49 +0530

    Updated Index.js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,7 @@ import "./scss/custom.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const Index = () => {
-  return (
-    <>
-      <App />
-      {/* <div className="back-to-top">
-        <i className="fas fa-angle-up" />
-      </div> */}
-    </>
-  );
-};
-
-ReactDOM.render(<Index />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
